4f70df2066aa8114c7e73581b984e7f8b6832f2a
Saurabh-827 <saurabhrabta@gmail.com>
2025-07-19 23:50:57 +0530

    Add Bootstrap and refactor App component; update .gitignore and package files

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,8 @@
 function Message() {
 	//JSX javaScript XML
+	const name = "Rabta";
+	// const name = "";
+	if (name) return <h1>Hello {name}</h1>;
 	return <h1>Hello World</h1>;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Using non-null assertion
+// createRoot(document.getElementById('root')!).render(...)
+
+// Using type assertion
+// createRoot(document.getElementById('root') as HTMLElement).render(...)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
