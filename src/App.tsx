831249fc8c6fb98070cadf435d2646c58d39dc3e
Saurabh-827 <saurabhrabta@gmail.com>
2025-07-21 00:08:07 +0530

    Refactor ListGroup component to accept props for items, heading, and onSelectItem; update index.html for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import ListGroup from "./components/ListGroup";
 function App() {
+	let items = ["New York", "London", "Delhi", "Paris", "Dubai", "Basti"];
+	const handleSelectItem = (item: string) => {
+		console.log(item);
+	};
 	return (
 		<div>
-			<ListGroup />
+			<ListGroup
+				items={items}
+				heading="Cities"
+				onSelectItem={handleSelectItem}
+			/>
 		</div>
 	);
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,49 @@
-// import { Fragment } from "react";
-function ListGroup() {
-	// You can use a <div> or a <Fragment> or <></> for returning multiple points
+// import { Fragment } from "react"; if using <Fragement/>
+
+import { useState } from "react";
+interface ListGroupProps {
+	items: string[];
+	heading: string;
+	onSelectItem: (item: string) => void;
+}
+
+// import type { MouseEvent } from "react";
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+	// let items = ["New York", "London", "Delhi", "Paris", "Dubai"];
+	// items = [];
+	//event handling
+	// const handleClick = (event: MouseEvent) => console.log(event);
+
+	const [selectedIndex, setSelectedIndex] = useState(-1);
+
 	return (
+		// You can use a <div> or a <Fragment> or <></> for returning multiple points
 		<>
-			<h1>List Group</h1>
+			<h1>{heading}</h1>
+			{
+				/* {if (condition) {}}  we cant use statements in jsx we can only use expressions (variables,function calls, operators, Araay methods, Object property access) in the jsx*/
+				// items.length === 0 ? <p>No Item Found</p> : null
+			}
+			{items.length === 0 && <p>No Item Found</p>}
+
 			<ul className="list-group">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-				<li className="list-group-item">A fourth item</li>
-				<li className="list-group-item">And a fifth one</li>
+				{items.map((item, index) => (
+					<li
+						className={
+							selectedIndex === index
+								? "list-group-item active"
+								: "list-group-item"
+						}
+						key={item}
+						onClick={() => {
+							setSelectedIndex(index);
+							onSelectItem(item);
+						}}
+						// onClick={handleClick}
+					>
+						{item}
+					</li>
+				))}
 			</ul>
 		</>
 	);
