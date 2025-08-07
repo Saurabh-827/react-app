76b65cdd2b84180f8b69c0fdb1dbc7f9a807708a
Saurabh-827 <saurabhrabta@gmail.com>
2025-08-07 20:45:16 +0530

    Add Alert component and update App to use it; comment out ListGroup implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+// import ListGroup from "./components/ListGroup";
 function App() {
-	let items = ["New York", "London", "Delhi", "Paris", "Dubai", "Basti"];
-	const handleSelectItem = (item: string) => {
-		console.log(item);
-	};
+	// let items = ["New York", "London", "Delhi", "Paris", "Dubai", "Basti"];
+	// const handleSelectItem = (item: string) => {
+	// 	console.log(item);
+	// };
 	return (
 		<div>
-			<ListGroup
+			{
+				/* <ListGroup
 				items={items}
 				heading="Cities"
 				onSelectItem={handleSelectItem}
-			/>
+			/> */
+				<Alert>Hello world</Alert>
+			}
 		</div>
 	);
 }
