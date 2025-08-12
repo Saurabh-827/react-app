a34b135e87b7e8a01e9a3d2688dd5c37e8b959bf
Saurabh-827 <saurabhrabta@gmail.com>
2025-08-12 22:04:24 +0530

    Add styled-components for NewListGroup; implement item selection functionality

--- src/components/StyleListGroup/NewListGroup.tsx
+++ src/components/StyleListGroup/NewListGroup.tsx
@@ -1,20 +1,45 @@
 import styles from "./NewListGroup.module.css";
+import { useState } from "react";
+import styled from "styled-components";
+interface ListItemsProps {
+	active: boolean;
+}
 interface GroupProps {
 	items: string[];
 	heading: string;
+	onSelectItem: (item: string) => void;
 }
 
-function NewListGroup({ items, heading }: GroupProps) {
+const List = styled.ul`
+	list-style: none;
+	background-color: aquamarine;
+	padding: 0;
+`;
+const ListItems = styled.li<ListItemsProps>`
+	padding: 5px 0;
+	background: ${(props) => (props.active ? "blue" : "none")};
+`;
+function NewListGroup({ items, heading, onSelectItem }: GroupProps) {
+	const [selectedIndex, setSelectedIndex] = useState(0);
 	return (
 		<>
 			<h1>{heading}</h1>
 			{items.length === 0 && <p>No items found</p>}
 			{/* <ul className="list-group"> */}
-			<ul className={styles["list-group"]}>
-				{items.map((item) => (
-					<li key={item}>{item}</li>
+			<List>
+				{items.map((item, index) => (
+					<ListItems
+						active={index === selectedIndex}
+						key={item}
+						onClick={() => {
+							setSelectedIndex(index);
+							onSelectItem(item);
+						}}
+					>
+						{item}
+					</ListItems>
 				))}
-			</ul>
+			</List>
 		</>
 	);
 }
